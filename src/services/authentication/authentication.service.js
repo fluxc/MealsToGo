f1129abdcbdf5c4166035874cce1eff62efa8808
fluxc <reflux_c@yahoo.com>
2024-03-12 15:38:15 +0000

    remove stuff

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -9,15 +9,8 @@ import {
 	signOut,
 } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { firebaseConfig } from '../../utils/env';
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyBnBPJcQKpCKSUcNFrw5C1ed2fcDcC4iDo',
-	authDomain: 'mealstogo-24008.firebaseapp.com',
-	projectId: 'mealstogo-24008',
-	storageBucket: 'mealstogo-24008.appspot.com',
-	messagingSenderId: '791468113227',
-	appId: '1:791468113227:web:1952932461622f0091fcce',
-};
 let loaded = null;
 let auth = null;
 if (!loaded) {
